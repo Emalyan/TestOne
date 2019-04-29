9d03b8832a18cd87c904f52e455b737a90c935e5
Ivan <brandy.bg@mail.ru>
2019-04-29 23:11:14 +0300

    InGitTestOne

---
+++ AuthenticationWebApiTest/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AuthenticationWebApiTest.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationWebApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        UserContext db;
+        public AccountController(UserContext context)
+        {
+            db = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(user);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return Ok(user);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AuthenticationWebApiTest/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationWebApiTest.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AuthenticationWebApiTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //добавляем строку подлючения
+            string connection = Configuration.GetConnectionString("DevConnect");
+            //добавляем контекст и поключаем строку подключения
+            services.AddDbContext<UserContext>(options => options.UseSqlServer(connection));
+
+            //устанавливаем конфигурацию подключения
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => 
+                {
+                    options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
+                });
+
+            services.AddMvc();
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {                
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
